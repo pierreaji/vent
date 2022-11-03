3fffd2f5a083c6f3652645b7a54801d1a41e993e
Pier <alrofiqy@gmail.com>
2022-11-03 10:40:01 +0700

    first commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+// Connect mongodb and start server
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+
+    })
+    .catch((err) => console.log(err))
