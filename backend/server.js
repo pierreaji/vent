0ac55d024413f115e22efc292a4e72ca4d3d476f
Pier <alrofiqy@gmail.com>
2022-11-03 10:52:47 +0700

    updates

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,21 @@ const cors = require('cors')
 
 const app = express()
 
+//Middleware
+app.use(express.json)
+app.use(express.urlencoded({
+    extended:false
+}))
+app.use(bodyParser.json)
+
 //routes
 app.get('/', (req, res) => {
     res.send('Home Page')
 })
 
 // Connect mongodb and start server
-mongoose
 const PORT = process.env.PORT || 5000
+mongoose
     .connect(process.env.MONGO_URL)
     .then(() => {
         
