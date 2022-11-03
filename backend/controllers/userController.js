059f8b4ff59aa513c90e9539a00b0c7533de7539
Pier <alrofiqy@gmail.com>
2022-11-03 16:09:40 +0700

    updates

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,41 @@
-const registerUser = async (req, res) => {
-    if (!req.body.email) {
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
+
+const registerUser = asyncHandler( async (req, res) => {
+    const {name, email, password} = req.body
+
+    //validation
+    if (!name || !email || !password) {
         res.status(400)
-        throw new Error('Please add an email')
+        throw new Error('Please fill in all required fields')
     }
-    res.send('Register User')
-}
+    if (password.length < 6) {
+        res.status(400)
+        throw new Error('Password must be have min 6 character')
+    }
+    //cek if user email already exist
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.status(400)
+        throw new Error('Email has already been registered!')
+    }
+
+    //create new user
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    if (user) {
+        const {_id, name, email, photo, bio} = user
+        res.status(201).json({
+            _id, name, email, photo, bio
+        })
+    } else {
+        throw new Error('Invalid user data')
+    }
+})
 
 module.exports = {
     registerUser,
